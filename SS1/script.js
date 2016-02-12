9250107919424ec815ecef33448751df9467a424
nhollerbach <nathanhollerbach@gmail.com>
2016-02-12 11:10:45 -0600

    Getting my shit back together

--- SS1/script.js
+++ SS1/script.js
@@ -1,9 +1,24 @@
 // Code goes here
-
-http://i.imgur.com/IkiLeXj.jpg
-
-
+var imageBackgroundArray = ["glowTop", "glowBottom","glowLeft","glowRight"];
 var colorArray = ["green", "blue", "teal", "yellow", "black", "orange", "aquamarine", "white"];
+
+
+
+function highlightOpenSessions(x) {
+  var logoSlide = document.getElementById("Top");
+  logoSlide.addEventListener("webkitAnimationEnd", function() {
+    
+    var logoBox = document.getElementById("logoBox");
+
+    for (i = 0; i < x; i++) {
+      var glowDiv = document.createElement('div');
+      glowDiv.className = 'glowBox';
+      glowDiv.id = imageBackgroundArray[i];
+      logoBox.appendChild(glowDiv);
+    };
+  }, false);
+}
+
 
 function buildAllSlices(x) {
   //First, remove all the existing elements from PizzaBox
